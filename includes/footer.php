e19dcf12b2bbc997c1291c4f755562c6467fa9c6
luijoc1 <luijoc75@gmail.com>
2025-05-27 15:53:14 -0500

    Proye v1

---
+++ includes/footer.php
@@ -0,0 +1,73 @@
+
+<link rel="stylesheet" href="wasathpp.css">
+<footer class="main-footer bg-blue" style="padding: 30px 0; color: #fff; background: #003366;">
+  <div class="container">
+    <div class="row">
+      <!-- Información de la empresa -->
+      <div class="col-sm-4">
+        <h4>ALMACÉN LOS ALMENDROS</h4>
+        <p>Repuestos de segunda mano, confiables y a bajo costo.</p>
+        <p><i class="fa fa-map-marker"></i> Dirección: Calle Principal del municipio de Fundacion Magdalena</p>
+        <p><i class="fa fa-phone"></i> Teléfono: +57 3045800522</p>
+        <p><i class="fa fa-envelope"></i> Email: contacto@almendros.com</p>
+      </div>
+      <!-- Enlaces útiles -->
+      <div class="col-sm-4">
+        <h4>Enlaces útiles</h4>
+        <ul style="list-style: none; padding-left: 0;">
+          <li><a href="index.php" style="color: #fff;">Inicio</a></li>
+          <li><a href="sobrenosotros.php" style="color: #fff;">Sobre Nosotros</a></li>
+          <li><a href="contacto.php" style="color: #fff;">Contacto</a></li>
+          <li><a href="productos.php" style="color: #fff;">Productos</a></li>
+        </ul>
+      </div>
+
+
+
+
+<!--<footer class="main-footer bg-blue" style>
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b></b>
+      </div>
+      <strong>ALMACEN LOS ALMENDROS</strong>
+      
+    </div>-->
+
+
+
+   
+<!-- Cargar SDK de Facebook para JavaScript -->
+<div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({2
+            xfbml            : true,
+            version          : 'v8.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!--Su código de complemento de chat-->
+      <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="112976387041659"
+        theme_color="#0084ff"
+        greeting_dialog_display= "fade"
+  logged_in_greeting="Hola Mucho Gusto ¿Cuál es su consulta?"
+  logged_out_greeting="Hola Mucho Gusto ¿Cuál es su consulta?">
+      </div>
+      <body>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<a href="https://api.whatsapp.com/send?phone=+573045800522&text=Hola%21%20necesito%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Los%20Productos." class="float" target="_blank">
+<i class="fa fa-whatsapp my-float"></i>
+</a>
+</body>
+</footer>
